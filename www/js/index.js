aa7ade4b765c752e150c3e559c8e2715784c9cd2
ganeshgadam <gadam_ganesh.ghrcecs@raisoni.net>
2018-07-17 09:47:05 +0530

    zs

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+var app = document.getElementById('demo');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('altered!')
+    .start();
